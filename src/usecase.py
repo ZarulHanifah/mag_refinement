322eb1615a479dbd47923d945fb34e07423af8e6
ZarulHanifah <zarulsaurus@gmail.com>
2026-01-23 16:55:58 +0800

    Mod usecase to check circular contig MAGs

--- src/usecase.py
+++ src/usecase.py
@@ -1,13 +1,67 @@
+import os
+import sys
+
+print(os.getcwd())
+sys.path.append('./src/')
+
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from pathlib import Path
 from pprint import pprint
-from typing import Optional
+from typing import Generator, Optional
+import subprocess
+from string import ascii_lowercase
 
 from tqdm import tqdm
 
-from models import Mag, SessionManager
+from models import ContigID, Mag, SessionManager
 
+
+def is_mag_single_circular(mag: Mag) -> bool:
+    return len(mag.contigids) == 1 and mag.contigids[0].is_circular()
+
+def is_mag_multiple_contigs_but_got_circular_contig(mag: Mag) -> bool:
+    return len(mag.contigids) > 1 and  any([ c.is_circular() for c in mag.contigids ])
+
+def get_mags_single_circular(mags: list[Mag]) -> Generator[Mag, None, None]:
+    for mag in mags:
+        if is_mag_single_circular(mag):
+            yield mag
+
+def get_mags_single_linear(mags: list[Mag]) -> Generator[Mag, None, None]:
+    for mag in mags:
+        if not is_mag_single_circular(mag):
+            yield mag
+
+def get_mags_multiple_contigs_but_with_circular_contig(mags: list[Mag]) -> Generator[Mag, None, None]:
+    for mag in mags:
+        if is_mag_multiple_contigs_but_got_circular_contig(mag):
+            yield mag
+
+def get_circular_contigs_from_mag(mag: Mag) -> Generator[ContigID, None, None]:
+    for contig in mag.contigids:
+        if contig.is_circular():
+            yield contig
+
+def split_ctg_fasta(mag: Mag):
+    seqtk_path = "/home/ahbui/Zarul/Software/miniconda3/envs/bio1/bin/seqtk"
+    
+    print(mag.name)
+
+    idx = 0
+    for ctg in get_circular_contigs_from_mag(mag):
+        print(ctg.name)
+        source_fasta = mag.fp
+        target_name = f"{mag.name}{ascii_lowercase[idx]}"
+        ctg_path = f"/tmp/{target_name}.id"
+        target_path = f"temp_path/{target_name}.fasta"
+
+        with open(ctg_path, "w") as f:
+            f.write(ctg.name)
+
+        with open(target_path, "w") as f_out:
+            subprocess.run([seqtk_path, "subseq", source_fasta, ctg_path], stdout=f_out)
+        idx += 1
 
 def check_mag_depths(mag: Mag, depth_cutoff: int) -> tuple[Mag, bool, bool]:
     is_above_ind = mag.average_coverage_per_sample(mag.long_sample) >= depth_cutoff
@@ -56,20 +110,18 @@ def get_hq_mags_and_above_cutoff(sesh: SessionManager,
 
     return above_ind_cutoff, above_total_cutoff, below_cutoff
 
-def main():
+if __name__ == "__main__":
     summary_repo = Path('../input_folder/summary.tsv')
-    mag_dir = Path('../input_folder//dereplicated_genomes/')
+    mag_dir = Path('../input_folder/dereplicated_genomes/')
     abund_dir = Path('../input_folder/mtbt_gen_depth/')
 
     sesh = SessionManager(summary_repo, mag_dir, abund_dir)
     above_ind_cutoff, above_total_cutoff, below_cutoff = get_hq_mags_and_above_cutoff(sesh, 30, 6)
 
-    for i in [above_ind_cutoff, above_total_cutoff, below_cutoff]:
-        i = [m.name for m in i]
-        varname = f"{i=}".split("=")[0]
-        print(varname.center(30, "="))
-        pprint(i)
+    print("MAGs with multiple contigs but with circular contig:")
+    for mag in get_mags_multiple_contigs_but_with_circular_contig(above_ind_cutoff):
+        print(f"MAG: {mag.name}, {mag.classification}")
+        for contig in get_circular_contigs_from_mag(mag):
+            print(f"  - Circular Contig: {contig.name}, Length: {contig.length}")
+        split_ctg_fasta(mag)
         print()
-
-if __name__ == "__main__":
-    main()
